92d99013ce9720f9187596617ac5d777d77bc293
Vincenzo Russo <v@artetecha.com>
2018-10-24 18:22:47 +0100

    Prepping for first npm release

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# postgraphile-artetecha-inflector
-Apply Artetecha's favourite rules to PostGraphile inflection
+# `postgraphile-artetecha-inflector`
+
+Override some of PostgraPhile's core inflectors with Artetecha's own favourite inflection rules.
+
+Implemented so far:
+
+### `allRows`:
+
+Given a type, e.g. `Metadata`:
+
+- Core `allRows` would be `allMetadata`
+- Artetecha's is `metadataConnection`
+
+### `allRowsSimple`
+
+Given a type, e.g. `Metadata`:
+
+- Core `allRowsSimple` would be `allMetadataList`
+- Artetecha's is `metadata`
--- index.js
+++ index.js
@@ -1,18 +1,41 @@
-module.exports = function ThinSchemaInflectorPlugin(builder, { pgSimpleCollections }) {
-  const hasSimpleCollections = pgSimpleCollections === "only" || pgSimpleCollections === "both";
-  if (!hasSimpleCollections) {
-    throw new Error("This plugin is designed for simple collections only.");
-  }
-
+module.exports = function PgArtetechaInflectorPlugin(builder) {
   builder.hook("inflection", inflection => {
+    inflection.smartPluralize = input => {
+      const singular = inflection.singularize(input);
+      const plural = inflection.pluralize(singular);
+      if (plural == singular) {
+        if (
+          plural.endsWith("ch") ||
+          plural.endsWith("s") ||
+          plural.endsWith("sh") ||
+          plural.endsWith("x") ||
+          plural.endsWith("z")
+        ) {
+          return plural + "es";
+        } else {
+          if (plural.endsWith("y")) {
+            return plural.slice(0, -1) + "ies";
+          } else {
+            return plural + "s";
+          }
+        }
+      }
+      return plural;
+    };
     return {
       ...inflection,
       allRowsSimple(table) {
-        return this.camelCase(`${this.pluralize(this._singularizedTableName(table))}`);
+        return this.camelCase(
+          `${this.smartPluralize(this._singularizedTableName(table))}`
+        );
       },
       allRows(table) {
-        return this.camelCase(`${this.pluralize(this._singularizedTableName(table))}-connection`);
-      },
+        return this.camelCase(
+          `${this.smartPluralize(
+            this._singularizedTableName(table)
+          )}-connection`
+        );
+      }
     };
   });
 };
